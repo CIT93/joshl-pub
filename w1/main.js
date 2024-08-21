164c5454511e92ad3c814d12d5c63026c25d435d
Josh Lee <ijoshlee99@gmail.com>
2024-08-21 18:33:57 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('hello world!')
+console.log("hello from the inside the main.js file");
+
+//let myVar;
+
+const myVar = 42;
+const myVarType = typeof(myVar);
+console.log("myVarType is " + myVarType);
+console.log(`myVarType is ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "string") {
+        console.log(`will 11 this one run?`);
+    } else {
+        console.log(`will 13 this one run?`);
+    }
+}
+
+runNow();
+runNow();
