ff10a4a235e5f2e48e76bf12ca468a926a0bc905
Josh Lee <ijoshlee99@gmail.com>
2024-09-01 07:22:53 +0000

    setup and display done

--- w3/main.js
+++ w3/main.js
@@ -1,3 +1,19 @@
+function determineHouseSizePts(size) {
+  if (size === "large") {
+    carbonFootprintPoints = carbonFootprintPoints + 10
+  } else if (size === "medium") {
+    carbonFootprintPoints = carbonFootprintPoints + 7
+  } else if (size === "small") {
+    carbonFootprintPoints = carbonFootprintPoints + 4
+  } else if (size === "apt") {
+    carbonFootprintPoints = carbonFootprintPoints + 2
+  }else {
+    console.log("no update to points");
+  }
+  console.log(
+    `Based on the number of house size of ${size} the points would be ${carbonFootprintPoints}.`);
+}
+
 function determineHouseHoldPts(numberInHousehold) {
   console.log("Inside the function");
   if (numberInHousehold === 1) {
@@ -23,41 +39,33 @@ function determineHouseHoldPts(numberInHousehold) {
 
 let carbonFootprintPoints = 0;
 
-//const numberInHousehold = 3;
-
 //global scope
 
-determineHouseHoldPts(3);
-determineHouseHoldPts(4);
+determineHouseHoldPts(5);
+determineHouseSizePts("apt");
 
-//CF HOUSEHOLD AND FUNCTIONS:
-//Consider the size of your home
-//If you have a large house, then add 10 points to your score.
-//If you have a medium-sized house, then add 7 points.
-//If you have a small house, then add 4 points.
-//If you live in an apartment, then add 2 points.
-
-function determineHouseSizePts(houseSize) {
- console.log("Inside HouseSize Function");
-   switch (houseSize) {
-       case "large":
-           carbonFootprintPoints = carbonFootprintPoints + 10;
-           console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
-           break;
-       case "medium":
-           carbonFootprintPoints = carbonFootprintPoints + 7;
-          console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
-           break;
-       case "small":
-           carbonFootprintPoints = carbonFootprintPoints + 4;
-           console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
-           break;
-       case "apartment":
-           carbonFootprintPoints = carbonFootprintPoints + 2;
-         console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
-            break;
-           default:
-             console.log("no update to points from house size");
-    }
-}
-determineHouseSizePts("large")
+//Below is my code, im putting it as comments to follow the code along lecture
+//function determineHouseSizePts(houseSize) {
+// console.log("Inside HouseSize Function");
+//   switch (houseSize) {
+//       case "large":
+//           carbonFootprintPoints = carbonFootprintPoints + 10;
+//           console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
+//           break;
+//       case "medium":
+//           carbonFootprintPoints = carbonFootprintPoints + 7;
+//          console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
+//           break;
+//       case "small":
+//           carbonFootprintPoints = carbonFootprintPoints + 4;
+//           console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
+//           break;
+//       case "apartment":
+//           carbonFootprintPoints = carbonFootprintPoints + 2;
+//         console.log(`Based on the number of members of the household and your house size being ${houseSize}, the points would be ${carbonFootprintPoints}.`);
+//            break;
+//           default:
+//             console.log("no update to points from house size");
+//    }
+//}
+//determineHouseSizePts("large")
